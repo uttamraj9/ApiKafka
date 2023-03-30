bc355f274b062703163760a66a219da6ec5a0770
uttamraj9 <uttamraj9@gmail.com>
2023-03-30 17:42:58 +0100

    updated POM again 31

--- src/main/scala/kafka/SendAPIToKafka.scala
+++ src/main/scala/kafka/SendAPIToKafka.scala
@@ -1,70 +1,27 @@
 package kafka
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.Trigger
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import requests._
+
 
 class SendAPIToKafka {
   def readFromApiAndProduceToKafka(): Unit = {
     val spark = SparkSession.builder()
-      .appName("API Reader")
-      .master("local[*]")
+      .appName("MyApp")
+      .master("local[*]") // Use all available cores on the local machine
       .getOrCreate()
-
-    val endpoint = "https://covid-193.p.rapidapi.com/statistics?country=UK"
+    import spark.implicits._
+    val apiUrl = "https://covid-193.p.rapidapi.com/statistics?country=UK"
     val headers = Map(
       "X-RapidAPI-Host" -> "covid-193.p.rapidapi.com",
       "X-RapidAPI-Key" -> "9a99f8903cmsh9e882a8f6e4b0cbp1f2fefjsn53f7c6e89019"
     )
+    val response = get(apiUrl, headers = headers)
+    val total = response.text()
+    val dfFromText = spark.read.json(Seq(total).toDS)
+    val kafka_msg = dfFromText.select(to_json(struct($"response"))).toDF("value")
 
-    val options = Map(
-      "delimiter" -> ":",
-      "url" -> endpoint,
-      "header_x_rapidapi" -> s"${headers("X-RapidAPI-Host")}:${headers("X-RapidAPI-Key")}"
-    )
+    kafka_msg.selectExpr("value").write.format("kafka").option("kafka.bootstrap.servers", "ip-172-31-3-80.eu-west-2.compute.internal:9092").option("topic", "topic1").save()
 
-    val covidSchema = StructType(Array(
-      StructField("continent", StringType),
-      StructField("country", StringType),
-      StructField("population", LongType),
-      StructField("cases", StructType(Array(
-        StructField("new", StringType),
-        StructField("active", LongType),
-        StructField("critical", LongType),
-        StructField("recovered", LongType),
-        StructField("total", LongType)
-      ))),
-      StructField("deaths", StructType(Array(
-        StructField("new", StringType),
-        StructField("total", LongType)
-      ))),
-      StructField("tests", StructType(Array(
-        StructField("total", LongType)
-      ))),
-      StructField("day", StringType),
-      StructField("time", StringType)
-    ))
-
-    val apiData = spark.read
-      .format("csv")
-      .options(options)
-      .load()
-      .select(from_json(col("value"), covidSchema).alias("data"))
-      .selectExpr("data.*")
-
-    val kafkaData = apiData
-      .select(to_json(struct(col("*"))).alias("value"))
-      .selectExpr("CAST(value AS STRING)")
-
-    val kafkaSink = kafkaData
-      .writeStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "ip-172-31-3-80.eu-west-2.compute.internal:9092")
-      .option("topic", "topic1")
-      .option("checkpointLocation", "/tmp/checkpoints")
-      .trigger(Trigger.ProcessingTime("30 seconds"))
-      .start()
-
-    kafkaSink.awaitTermination()
   }
 }
