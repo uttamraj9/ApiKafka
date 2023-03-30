01ba2728d9b4bd7fc5ce1b752082a8118e083092
uttamraj9 <uttamraj9@gmail.com>
2023-03-30 18:20:05 +0100

    updated POM again 31

--- src/main/scala/kafka/SendAPIToKafka.scala
+++ src/main/scala/kafka/SendAPIToKafka.scala
@@ -5,23 +5,23 @@ import requests._
 
 
 class SendAPIToKafka {
-  def readFromApiAndProduceToKafka(): Unit = {
+  def readFromApiAndProduceToKafka(topic: String, brokers: String, headers: Map[String, String], url: String): Unit = {
     val spark = SparkSession.builder()
       .appName("MyApp")
       .master("local[*]") // Use all available cores on the local machine
       .getOrCreate()
     import spark.implicits._
-    val apiUrl = "https://covid-193.p.rapidapi.com/statistics?country=UK"
+/*    val apiUrl = "https://covid-193.p.rapidapi.com/statistics?country=UK"
     val headers = Map(
       "X-RapidAPI-Host" -> "covid-193.p.rapidapi.com",
       "X-RapidAPI-Key" -> "9a99f8903cmsh9e882a8f6e4b0cbp1f2fefjsn53f7c6e89019"
-    )
-    val response = get(apiUrl, headers = headers)
+    )*/
+    val response = get(url, headers = headers)
     val total = response.text()
     val dfFromText = spark.read.json(Seq(total).toDS)
     val kafka_msg = dfFromText.select(to_json(struct($"response"))).toDF("value")
 
-    kafka_msg.selectExpr("value").write.format("kafka").option("kafka.bootstrap.servers", "ip-172-31-3-80.eu-west-2.compute.internal:9092").option("topic", "topic1").save()
+    kafka_msg.selectExpr("value").write.format("kafka").option("kafka.bootstrap.servers", brokers).option("topic", topic).save()
 
   }
 }
--- src/main/scala/kafka/SendMessage.scala
+++ src/main/scala/kafka/SendMessage.scala
@@ -5,9 +5,8 @@ object SendMessage {
     val topic = "topic1"
     val brokers = "ip-172-31-3-80.eu-west-2.compute.internal:9092,ip-172-31-5-217.eu-west-2.compute.internal:9092"
     val headers = Map("X-RapidAPI-Host" -> "covid-193.p.rapidapi.com", "X-RapidAPI-Key" -> "9a99f8903cmsh9e882a8f6e4b0cbp1f2fefjsn53f7c6e89019")
-    val url = "https://covid-193.p.rapidapi.com/statistics"
-    val country = "UK"
+    val url = "https://covid-193.p.rapidapi.com/statistics?country=uk"
     val sc = new SendAPIToKafka()
-    sc.readFromApiAndProduceToKafka()
+    sc.readFromApiAndProduceToKafka(topic, brokers, headers, url)
   }
 }
